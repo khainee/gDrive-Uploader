c86895601e5ecbc6b511a66b30e07e8e8d3d395c
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-18 17:41:40 +0630

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -7,17 +7,15 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def _start(client, message):
     client.send_message(chat_id=message.chat.id,
         text=tr.START_MSG.format(message.from_user.mention),
-        reply_to_message_id=message.message_id
-    )
+        reply_to_message_id=message.message_id)
 
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['help']), group=2)
 def _help(client, message):
     client.send_message(chat_id=message.chat.id,
-        text= r.HELP_MSG[1],
-        reply_markup \=InlineKeyboardMarkup(map(1)),
-        reply_to_message_id = message.message_id
-    )
+        text=tr.HELP_MSG[1],
+        reply_markup=InlineKeyboardMarkup(map(1)),
+        reply_to_message_id=message.message_id 
 
 help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
 
